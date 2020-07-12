56ad59d24267f30aaa7efbb5747842ec7ad576f7
Victor Fernandez <vmfs99@gmail.com>
2020-07-12 15:54:33 -0400

    really?

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,6 +1,5 @@
 import React, { Component } from "react";
 import { Link } from "react-router-dom";
-import Contact from "./Contact";
 
 export default class Home extends Component {
   componentDidMount() {
