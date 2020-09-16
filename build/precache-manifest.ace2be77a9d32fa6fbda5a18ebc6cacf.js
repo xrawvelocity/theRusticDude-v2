16874d4e4fd5be0d74da5f6c9ca3fb1a101010c3
Victor Fernandez <xrawvelocity@gmail.com>
2020-09-16 00:15:53 -0400

    build css and js

---
+++ build/precache-manifest.ace2be77a9d32fa6fbda5a18ebc6cacf.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9f3ef0a2bc4948584da5a52f678ff79f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1fccd1abdc5d50b66b9d",
+    "url": "/static/css/main.f5129348.chunk.css"
+  },
+  {
+    "revision": "d68456959540d000fa96",
+    "url": "/static/js/2.1bc3233d.chunk.js"
+  },
+  {
+    "revision": "6331f70a3ea8aafd2e3fca1343e5ed47",
+    "url": "/static/js/2.1bc3233d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1fccd1abdc5d50b66b9d",
+    "url": "/static/js/main.8a8e6a78.chunk.js"
+  },
+  {
+    "revision": "1bb895b2429444a647a3",
+    "url": "/static/js/runtime-main.09444656.js"
+  }
+]);
--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.71e1ab029b34a18a43933411b1dc2895.js"
+  "/precache-manifest.ace2be77a9d32fa6fbda5a18ebc6cacf.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -35,5 +35,5 @@ workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
 workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
-  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
